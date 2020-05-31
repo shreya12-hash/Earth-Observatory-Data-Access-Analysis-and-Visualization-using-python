8f6b2f480dc926568c45f6e185b79a25ca4b2979
shreya12-hash <64973845+shreya12-hash@users.noreply.github.com>
2020-05-31 05:33:02 +0530

    Update NAIP Multispectral Remote Sensing Data access ,analysis and visualization.py

--- NAIP Multispectral Remote Sensing Data access ,analysis and visualization.py
+++ NAIP Multispectral Remote Sensing Data access ,analysis and visualization.py
@@ -5,3 +5,19 @@ NAIP is a great source of high resolution imagery across the United States. NAIP
 One last step before we can search and download sentinel 2 images is to create a footprint from the nReservegeometry. Here I've used Shapely Python library since our data is in Shapefiles and read it already as Geopandas GeodataFrame. (Now one thing if we have Geojson data, Sentinelsat provides a handy way to convert our data into a proper format in the query).
 
 ###Open NAIP Data in Python
+
+#Next, I've used NAIP imagery for the Coldsprings fire study area in Colorado. To work with multi-band raster data we have to use the rasterio and geopandas packages. I've also used the plot module from the earthpy package for raster plotting.
+
+`import matplotlib.pyplot as plt`
+
+`import numpy as np`
+
+`import rasterio as rio`
+
+`import geopandas as gpd`
+
+`import earthpy as et`
+
+`import earthpy.plot as ep`
+
+`import earthpy.spatial as es`
